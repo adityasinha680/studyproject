1a654f926c4b4ec9438fdfa6483e8599b162a19d
Adichi <adityasinha680@gmail.com>
2020-09-29 01:01:45 +0530

    add courses and modify courses updated

--- studyapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Course (models.Model):
-	course_title = models.CharField(max_length=100)
-	institute_name = models.CharField(max_length=100)
-	course_desc =models.CharField(max_length=300)
